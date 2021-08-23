93eeec5cc4dc9771dba269bd2612b10d40ae5843
oswaldo garcia <garcia.oswaldo1000@gmail.com>
2021-08-23 10:17:17 -0500

    first commit

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums (
+                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                        artist VARCHAR(50) DEFAULT 'NONE',
+                        name  VARCHAR(100) NOT NULL,
+                        release_date TEXT NOT NULL,
+                        sales int UNSIGNED NOT NULL ,
+                        genre VARCHAR (100),
+                        PRIMARY KEY (id)
+);
+show tables;
+DESCRIBE albums;
